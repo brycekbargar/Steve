9c7bf0e6fbf0339b60b4558b3acec5438c356efb
Bryce <brycekbargar@gmail.com>
2015-11-29 16:38:11 -0600

    RGR Print the usage info when no commands are passed

--- test/steve.js
+++ test/steve.js
@@ -1,10 +1,21 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
 
-var steve = require('./../lib/steve.js');
+var optimist = require('optimist');
+
+var steve = require('./../lib/steve.js')(function(){}, function(){});
 
 describe('For the steve executable', function() {
-  it('expect to pass', function(){
-    steve();
-    expect(1).to.equal(1);
+  describe('expect the usage to be displayed when', function(){
+    beforeEach('Setup spy', function(){
+      this.showHelpSpy = sinon.spy(optimist, 'showHelp');
+    });
+    afterEach('Teardown spy', function(){
+      optimist.showHelp.restore();
+    });
+    it('no commands are passed', function(){
+      steve();
+      expect(this.showHelpSpy).to.have.been.calledOnce;
+    });
   });
 });
