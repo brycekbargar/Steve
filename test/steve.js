a38c93323a981b377be2f0e7596ae5795975611d
Bryce <brycekbargar@gmail.com>
2015-11-29 17:25:51 -0600

    RGR Test all the usage display cases

--- test/steve.js
+++ test/steve.js
@@ -1,20 +1,29 @@
-var expect = require('chai').expect;
+var proxyquire =  require('proxyquire');
 var sinon = require('sinon');
+var expect = require('chai').use(require('sinon-chai')).expect;
 
-var optimist = require('optimist');
+var optimist = require('optimist')();
 
-var steve = require('./../lib/steve.js')(function(){}, function(){});
+var steve = proxyquire('./../lib/steve.js', { 'optimist': optimist })(function(){}, function(){});
 
 describe('For the steve executable', function() {
-  describe('expect the usage to be displayed when', function(){
+  describe('expect the usage to be displayed when passed', function(){
     beforeEach('Setup spy', function(){
       this.showHelpSpy = sinon.spy(optimist, 'showHelp');
     });
     afterEach('Teardown spy', function(){
       optimist.showHelp.restore();
     });
-    it('no commands are passed', function(){
-      steve();
+    it('no commands', function(){
+      steve([]);
+      expect(this.showHelpSpy).to.have.been.calledOnce;
+    });
+    it('too many commands', function(){
+      steve(['init', 'start']);
+      expect(this.showHelpSpy).to.have.been.calledOnce;
+    });
+    it('an unknown command', function(){
+      steve(['blaargh']);
       expect(this.showHelpSpy).to.have.been.calledOnce;
     });
   });
