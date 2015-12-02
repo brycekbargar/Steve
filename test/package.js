066af4ee6469e6d100ed9d0e2d41587bc9514c15
Bryce <brycekbargar@gmail.com>
2015-12-01 22:43:32 -0600

    RGR Return error exit value

--- test/package.js
+++ test/package.js
@@ -10,7 +10,7 @@ describe('For the setup command', function() {
   beforeEach('Setup package', function(){
     this.package = proxyquire('./../lib/package.js', proxyquireStubs);
   });
-  describe('when interacting with the initialize.ck file expect', function(){
+  describe('when interacting with the initialize.ck file', function(){
     beforeEach('Setup spies', function(){
       this.initializeFileStub = proxyquireStubs['chuck-steve-initialize-file'] = stub();
       this.consoleErrorStub = stub(console, 'error');
@@ -18,28 +18,32 @@ describe('For the setup command', function() {
     afterEach('Teardown spies', function(){
       console.error.restore();
     });
-    it('the file to be loaded', function(){
+    it('expect the file to be loaded', function(){
       this.package();
       expect(this.initializeFileStub).to.have.been.calledOnce;
       expect(this.initializeFileStub).to.have.been.calledWith('./initialize.ck');
     });
-    describe('if there are errors', function(){
+    describe('and there are errors', function(){
       beforeEach('Setub spies', function(){
         this.initializeError = new Error();
         this.initializeFileStub.callsArgWith(1, this.initializeError);
       });
-      it('the user to be alerted', function() {
+      it('expect the user to be alerted', function() {
         this.package();
         expect(this.consoleErrorStub).to.have.been.calledOnce;
         expect(this.consoleErrorStub).to.have.been.calledWith(this.initializeError);
       });
-      it('to not continue', function() {
+      it('expect to not continue', function() {
         var packageFolderSpy = proxyquireStubs['chuck-steve-package-folder'] = spy();
         this.package();
         expect(packageFolderSpy).to.not.have.been.called;
       });
+      it('expect to exit with a 1', function() {
+        var exitValue = this.package();
+        expect(exitValue).to.equal(1);
+      });
     });
-    it('the user to not be alerted if things are ok', function() {
+    it('and there are no errors expect silence', function() {
       this.initializeFileStub.callsArgWith(1, null);
       this.package();
       expect(this.consoleErrorStub).to.not.have.been.called;
