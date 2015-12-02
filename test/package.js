2e7c0aaf13837bdc5d8743f27e4887c110a458e0
Bryce <brycekbargar@gmail.com>
2015-12-01 21:19:15 -0600

    RGR Load the initialize-file package

--- test/package.js
+++ test/package.js
@@ -1,10 +1,20 @@
+var proxyquire = require('proxyquire').noCallThru();
+var spy = require('sinon').spy;
+var expect = require('chai').use(require('sinon-chai')).expect;
+
+var proxyquireStubs = {};
+
 describe('For the setup command', function() {
   beforeEach('Setup package', function(){
-    this.package = require('./../lib/package.js');
+    this.package = proxyquire('./../lib/package.js', proxyquireStubs);
   });
-  describe('expect it', function(){
-    it('to pass', function(){
+  describe('when interacting with the initialize.ck file expect', function(){
+    beforeEach('Setup spies', function(){
+      this.initializeFileSpy = proxyquireStubs['chuck-steve-initialize-file'] = spy();
+    });
+    it('to load the file', function(){
       this.package();
+      expect(this.initializeFileSpy).to.have.been.calledOnce;
     });
   });
 });
