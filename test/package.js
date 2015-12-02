c570bcd5449d6cc22b13327f68fddc586eb0f4b7
Bryce <brycekbargar@gmail.com>
2015-12-01 22:51:29 -0600

    RGR Load the package folder

--- test/package.js
+++ test/package.js
@@ -7,17 +7,19 @@ var expect = require('chai').use(require('sinon-chai')).expect;
 var proxyquireStubs = {};
 
 describe('For the setup command', function() {
+  beforeEach('Setup spies', function() {
+    this.consoleErrorStub = stub(console, 'error');
+    this.initializeFileStub = proxyquireStubs['chuck-steve-initialize-file'] = stub();
+    this.initializeFileStub.callsArgWith(1, null);
+    this.packageFolderSpy = proxyquireStubs['chuck-steve-package-folder'] = spy();
+  });
+  afterEach('Teardown spies', function(){
+    console.error.restore();
+  });
   beforeEach('Setup package', function(){
     this.package = proxyquire('./../lib/package.js', proxyquireStubs);
   });
   describe('when interacting with the initialize.ck file', function(){
-    beforeEach('Setup spies', function(){
-      this.initializeFileStub = proxyquireStubs['chuck-steve-initialize-file'] = stub();
-      this.consoleErrorStub = stub(console, 'error');
-    });
-    afterEach('Teardown spies', function(){
-      console.error.restore();
-    });
     it('expect the file to be loaded', function(){
       this.package();
       expect(this.initializeFileStub).to.have.been.calledOnce;
@@ -34,9 +36,8 @@ describe('For the setup command', function() {
         expect(this.consoleErrorStub).to.have.been.calledWith(this.initializeError);
       });
       it('expect to not continue', function() {
-        var packageFolderSpy = proxyquireStubs['chuck-steve-package-folder'] = spy();
         this.package();
-        expect(packageFolderSpy).to.not.have.been.called;
+        expect(this.packageFolderSpy).to.not.have.been.called;
       });
       it('expect to exit with a 1', function() {
         var exitValue = this.package();
@@ -47,6 +48,12 @@ describe('For the setup command', function() {
       this.initializeFileStub.callsArgWith(1, null);
       this.package();
       expect(this.consoleErrorStub).to.not.have.been.called;
+    });
+  });
+  describe('when interacting with the package folder', function(){
+    it('expect the folder to be loaded', function(){
+      this.package();
+      expect(this.packageFolderSpy).to.have.been.calledOnce;
     });
   });
 });
