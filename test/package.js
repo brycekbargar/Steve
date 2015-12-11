16316121cbad9207d540355610ba0176e382fb72
Bryce <brycekbargar@gmail.com>
2015-12-10 19:25:34 -0600

    RGR Use promises for the package command

--- test/package.js
+++ test/package.js
@@ -1,31 +1,38 @@
 'use strict';
 
 const proxyquire = require('proxyquire').noCallThru();
-const sinon = require('sinon');
-const spy = sinon.spy;
-const stub = sinon.stub;
-const expect = require('chai').use(require('sinon-chai')).expect;
+const Promise = require('bluebird');
+require('sinon-as-promised')(Promise);
+const stub = require('sinon').stub;
+const expect = require('chai')
+    .use(require('sinon-chai'))
+    .use(require('chai-as-promised'))
+    .expect;
 
-const proxyquireStubs = {};
+const proxyquireStubs = {
+  bluebird: Promise
+};
 
-describe('For the setup command', () => {
+describe('For the package command', () => {
   beforeEach('Setup spies', () => {
-    this.consoleErrorStub = stub(console, 'error');
+    (this.initializeFileStub =
+      proxyquireStubs['chuck-steve-initialize-file'] =
+      stub())
+      .returns({
+        getFilePaths: this.getFilePathsStub = stub()
+      });
+    this.getFilePathsStub.resolves(this.filePaths = ['', '', '']);
 
-    this.initializeFileStub = proxyquireStubs['chuck-steve-initialize-file'] = stub();
-    this.filePaths = ['','',''];
-    this.initializeFileStub.callsArgWith(1, null, this.filePaths);
-
-    this.packageFolderStub = proxyquireStubs['chuck-steve-package-folder'] = stub();
-    this.packageFolderClearSpy = spy();
-    this.packageFolderAddSpy = spy();
-    let packageFolder = {
-      clear: this.packageFolderClearSpy,
-      add: this.packageFolderAddSpy
-    };
-    this.packageFolderStub.callsArgWith(1, null, packageFolder);
+    (this.packageFolderStub =
+      proxyquireStubs['chuck-steve-package-folder'] =
+      stub())
+      .returns({
+        clear: this.packageFolderClearStub = stub(),
+        add: this.packageFolderAddStub = stub()
+      });
+    this.packageFolderClearStub.resolves();
+    this.packageFolderAddStub.resolves();
   });
-  afterEach('Teardown spies', () => console.error.restore());
   beforeEach('Setup package', () =>
     this.package = proxyquire('./../lib/package.js', proxyquireStubs)
   );
@@ -33,70 +40,59 @@ describe('For the setup command', () => {
     it('expect the file to be loaded', () => {
       this.package();
       expect(this.initializeFileStub).to.have.been.calledOnce;
+      expect(this.initializeFileStub).to.have.been.calledWithNew;
       expect(this.initializeFileStub).to.have.been.calledWith('./initialize.ck');
     });
     describe('and there are errors', () => {
-      beforeEach('Setub spies', () => {
+      beforeEach('Setup spies', () => {
         this.initializeError = new Error();
-        this.initializeFileStub.callsArgWith(1, this.initializeError);
+        this.getFilePathsStub.rejects(this.initializeError);
       });
-      it('expect the user to be alerted', () => {
-        this.package();
-        expect(this.consoleErrorStub).to.have.been.calledOnce;
-        expect(this.consoleErrorStub).to.have.been.calledWith(this.initializeError);
+      it('expect it to be rejected', () => {
+        let pkg = this.package();
+        expect(pkg).to.eventually.be.rejectedWith(this.initializeError);
       });
       it('expect to not continue', () => {
-        this.package();
-        expect(this.packageFolderStub).to.not.have.been.called;
+        let pkg = this.package();
+        return pkg.catch(() => {
+          expect(this.packageFolderClearStub).to.not.have.been.called;
+          expect(this.packageFolderAddStub).to.not.have.been.called;
+        });
       });
-      it('expect to exit with a 1', () => {
-        let exitValue = this.package();
-        expect(exitValue).to.equal(1);
-      });
-    });
-    it('and there are no errors expect silence', () => {
-      this.package();
-      expect(this.consoleErrorStub).to.not.have.been.called;
     });
   });
   describe('when interacting with the package folder', () => {
     it('expect the folder to be loaded', () => {
       this.package();
       expect(this.packageFolderStub).to.have.been.calledOnce;
+      expect(this.packageFolderStub).to.have.been.calledWithNew;
       expect(this.packageFolderStub).to.have.been.calledWith('./package');
     });
-    describe('and there are errors', () => {
-      beforeEach('Setub spies', () => {
-        this.packageError = new Error();
-        this.packageFolderStub.callsArgWith(1, this.packageError);
+    describe('expect it to be rejected if there are errors', () => {
+      it('clearing the folder', () => {
+        let clearError = new Error();
+        this.packageFolderClearStub.rejects(clearError);
+        let pkg = this.package();
+        return pkg.catch(() => {
+          expect(pkg).to.be.rejectedWith(clearError);
+          expect(this.packageFolderAddStub).to.not.have.been.called;
+        });
       });
-      it('expect the user to be alerted', () => {
-        this.package();
-        expect(this.consoleErrorStub).to.have.been.calledOnce;
-        expect(this.consoleErrorStub).to.have.been.calledWith(this.packageError);
-      });
-      it('expect to not continue', () => {
-        this.package();
-        expect(this.packageFolderClearSpy).to.not.have.been.called;
-        expect(this.packageFolderAddSpy).to.not.have.been.called;
-      });
-      it('expect to exit with a 1', () => {
-        let exitValue = this.package();
-        expect(exitValue).to.equal(1);
+      it('adding to the folder', () => {
+        let addError = new Error();
+        this.packageFolderAddStub.rejects(addError);
+        let pkg = this.package();
+        return pkg.catch(() => expect(pkg).to.be.rejectedWith(addError));
       });
     });
     describe('and there are no errors', () => {
-      it('expect silence', () => {
-        this.package();
-        expect(this.consoleErrorStub).to.not.have.been.called;
-      });
       it('expect the package folder to be cleared', () => {
-        this.package();
-        expect(this.packageFolderClearSpy).to.have.been.called;
+        let pkg = this.package();
+        return pkg.then(() => expect(this.packageFolderClearStub).to.have.been.called);
       });
       it('expect each returned initialize path to be added to the package folder', () => {
-        this.package();
-        expect(this.packageFolderAddSpy.callCount).to.equal(this.filePaths.length);
+        let pkg = this.package();
+        return pkg.then(() => expect(this.packageFolderAddStub.callCount).to.equal(this.filePaths.length));
       });
     });
   });
