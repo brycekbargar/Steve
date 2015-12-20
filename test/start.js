70d34449147a41cd2b0b6abebe646b5850fecaa7
Bryce <brycekbargar@gmail.com>
2015-12-20 17:40:27 -0600

    RGR Throw if can't load the dependency tree

--- test/start.js
+++ test/start.js
@@ -17,8 +17,16 @@ describe('For the start command', () => {
       this.chuckVmStub =
       stub())
       .returns({
-        start: this.startStub = stub()
+        start: this.vmStartStub = stub()
       });
+    (proxyquireStubs['chuck-steve-dependency-tree'] =
+      this.dependencyTreeStub =
+      stub())
+      .returns({
+        load: this.dependencyTreeLoadStub = stub()
+      });
+    this.vmStartStub.resolves();
+    this.dependencyTreeLoadStub.resolves();
   });
   beforeEach('Setup command', () => this.start = proxyquire('./../lib/start.js', proxyquireStubs));
   it('expect it to create a new vm', () => {
@@ -26,10 +34,23 @@ describe('For the start command', () => {
     expect(this.chuckVmStub).to.have.been.calledOnce;
     expect(this.chuckVmStub).to.have.been.calledWithNew;
   });
+  it('expect it to create a new dependency tree', () => {
+    this.start();
+    expect(this.dependencyTreeStub).to.have.been.calledOnce;
+    expect(this.dependencyTreeStub).to.have.been.calledWithNew;
+  });
   it('expect it to be rejected if it can\'t start the vm', () => {
     let startError = new Error();
-    this.startStub.rejects(startError);
+    this.vmStartStub.rejects(startError);
     let start = this.start();
     expect(start).to.have.been.rejectedWith(startError);
+  });
+  describe('and the vm is started', () => {
+    it('expect it to be rejected if it can\'t load the dependency tree', () => {
+      let dependencyError = new Error();
+      this.dependencyTreeLoadStub.rejects(dependencyError);
+      let start = this.start();
+      expect(start).to.have.been.rejectedWith(dependencyError);
+    });
   });
 });
